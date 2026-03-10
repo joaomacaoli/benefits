139370c9eccaa34523a772320668a412dea0cd4a
João Macaoli <devjoaom@gmail.com>
2026-03-10 09:44:09 -0300

    criação do dto BeneficioRequestDTO

---
+++ backend-module/src/main/java/com/example/backend/dtos/BeneficioRequestDTO.java
@@ -0,0 +1,51 @@
+package com.example.backend.dtos;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public class BeneficioRequestDTO {
+
+    @NotNull(message = "Nome é obrigatório")
+    private String nome;
+
+    private String descricao;
+
+    @NotNull(message = "Valor é obrigatório")
+    private BigDecimal valor;
+
+    @NotNull(message = "Ativo é obrigatório")
+    private Boolean ativo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+}
